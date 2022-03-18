5da1e54f38e410387984dbba413c8a249ecbe5b5
Beck Lin <runbeck@163.com>
2022-03-18 21:27:10 +0800

    first commit

---
+++ message-streaming/src/main/java/com/bk/streaming/MessageStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.bk.streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageStreamingApplication.class, args);
+	}
+
+}
---
+++ message-streaming/src/test/java/com/bk/streaming/MessageStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bk.streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
